b33ca804fddc0a1be441493e6f1fc7e79c5e00d5
DQNEO <dqneoo@gmail.com>
2020-02-03 17:58:50 +0900

    Fix const name

--- 06_syscall.s
+++ 06_syscall.s
@@ -5,7 +5,7 @@
         .global main
 main:
 	lea .str1(%rip), %rcx # "hello\n"
-        mov $1, %rdi   # 1st argument: SYS_write (=1)
+        mov $1, %rdi   # 1st argument: __x64_sys_write (=1)
                        # see https://github.com/torvalds/linux/blob/v5.5/arch/x86/entry/syscalls/syscall_64.tbl#L12
 	mov $1, %rsi   # 2st argument: int file descriptor
 	mov %rcx, %rdx # 3nd argument: ptr to buffer
