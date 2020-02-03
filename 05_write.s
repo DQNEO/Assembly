e2b1b6c5ce9652cefbc21a93d7254ca635f5d80f
DQNEO <dqneoo@gmail.com>
2020-02-03 17:41:15 +0900

    sample to call the write(2) system call to print "hello\n"

---
+++ 05_write.s
@@ -0,0 +1,13 @@
+.str1:
+	.string "hello\n"
+
+	.text
+        .global main
+main:
+	lea .str1(%rip), %rcx # "hello\n"
+	mov $1, %rdi   # 1st argument: int file descriptor
+	mov %rcx, %rsi # 2nd argument: ptr to buffer
+        mov $6,  %rdx  # 3rd argument: size_t nbyte
+	mov $0, %rax
+	call write
+	ret
